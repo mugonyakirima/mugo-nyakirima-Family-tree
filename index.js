61bef5da704f2321a391f86d5f1e3f5e425f0ff6
mmmyykkeee <0mmyykkee@gmail.com>
2022-11-28 02:29:28 +0300

    gallery change

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
+document.getElementById("nyakirima-mugo").style.display = "none";
+document.getElementById("nyakirima-images").style.display = "none";
+document.getElementById("burial").style.display = "none";
+document.getElementById("ndambi").style.display = "none";
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+function hideElement() {
+  const elementHide = document.getElementById("nyakirima-images");
+  elementHide.style.display = "block";
+  document.getElementById("nyakirima-mugo").style.display = "none";
+  document.getElementById("burial").style.display = "none";
+  document.getElementById("ndambi").style.display = "none";
+}
+  
+function hideElement1() { 
+  const elementHide = document.getElementById("nyakirima-mugo");
+  elementHide.style.display = "block";
+  document.getElementById("nyakirima-images").style.display = "none";
+  document.getElementById("burial").style.display = "none";
+  document.getElementById("ndambi").style.display = "none";
+}
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+function hideElement2() {
+  const elementHide = document.getElementById("burial");
+  elementHide.style.display = "block";
+  document.getElementById("nyakirima-images").style.display = "none";
+  document.getElementById("nyakirima-mugo").style.display = "none";
+  document.getElementById("ndambi").style.display = "none";
+}
+
+function hideElement3() {
+  const elementHide = document.getElementById("ndambi");
+  elementHide.style.display = "block";
+  document.getElementById("nyakirima-images").style.display = "none";
+  document.getElementById("nyakirima-mugo").style.display = "none";
+  document.getElementById("burial").style.display = "none";
+}
